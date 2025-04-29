50b5450488d33db5704df8fb0d20ea7503065abe
Angelos Gustav Panou <128244012+angelospanou@users.noreply.github.com>
2025-04-29 10:32:50 +0200

    Update grpc.rs

--- src/grpc.rs
+++ src/grpc.rs
@@ -4,12 +4,15 @@ use crate::{
         ingest_service_server::{IngestService, IngestServiceServer},
         TxRequest, IngestResponse,
     },
+    #[cfg(feature = "sequencer")]
     config::SequencerAppState,
 };
 
+#[cfg(feature = "sequencer")]
 #[derive(Clone)]
 pub struct IngestSvc { pub state: SequencerAppState }
 
+#[cfg(feature = "sequencer")]
 #[tonic::async_trait]
 impl IngestService for IngestSvc {
     
@@ -26,6 +29,7 @@ impl IngestService for IngestSvc {
     }
 }
 
+#[cfg(feature = "sequencer")]
 pub fn make_server(state: SequencerAppState) -> IngestServiceServer<IngestSvc> {
     IngestServiceServer::new(IngestSvc { state })
 }
