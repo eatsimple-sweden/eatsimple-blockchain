78f64589dfcb54bad2c29f6b25cfaa09e03b9e9d
Angelos Gustav Panou <128244012+angelospanou@users.noreply.github.com>
2025-05-02 11:47:59 +0200

    Update sequencer.rs

--- src/bin/sequencer.rs
+++ src/bin/sequencer.rs
@@ -6,7 +6,9 @@ use eatsimple_blockchain::mode;
 async fn main() -> Result<()> {
     let cfg = config::SequencerConfig::load("config/sequencer.toml")?;
 
-    std::env::set_var("RUST_LOG", "rustls=trace,webpki=trace,tonic=debug");
+    unsafe {
+        std::env::set_var("RUST_LOG", "rustls=trace,webpki=trace");
+    }
     tracing_subscriber::fmt::init();
 
     rustls::crypto::aws_lc_rs::default_provider()
