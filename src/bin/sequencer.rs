40704b132c8f38491e5ea95f81dc7a8f5b10c335
Angelos Gustav Panou <128244012+angelospanou@users.noreply.github.com>
2025-05-02 11:37:14 +0200

    Update sequencer.rs

--- src/bin/sequencer.rs
+++ src/bin/sequencer.rs
@@ -6,7 +6,9 @@ use eatsimple_blockchain::mode;
 async fn main() -> Result<()> {
     let cfg = config::SequencerConfig::load("config/sequencer.toml")?;
 
-    rustls::crypto::aws_lc_rs::install_default();
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("failed to install default crypto provider");
     
     mode::sequencer::run(cfg).await
 }
