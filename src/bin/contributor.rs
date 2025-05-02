16acb5bc3e604c83b6c465e64522156665e897f5
Angelos Gustav Panou <128244012+angelospanou@users.noreply.github.com>
2025-05-02 12:11:46 +0200

    Update contributor.rs

--- src/bin/contributor.rs
+++ src/bin/contributor.rs
@@ -6,6 +6,11 @@ use eatsimple_blockchain::mode;
 async fn main() -> Result<()> {
     let contributor_cfg = config::ContributorConfig::load("config/contributor.toml")?;
 
+    unsafe {
+        std::env::set_var("RUST_LOG", "rustls=trace,webpki=trace");
+    }
+    tracing_subscriber::fmt::init();
+
     rustls::crypto::aws_lc_rs::default_provider()
         .install_default()
         .expect("failed to install default crypto provider");
