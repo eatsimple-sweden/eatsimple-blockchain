86064b98a5e52c1b9f2cf0d47688d1f800efbce8
Angelos Gustav Panou <128244012+angelospanou@users.noreply.github.com>
2025-05-02 12:43:38 +0200

    Update src.rs

--- src/handlers/enroll/src.rs
+++ src/handlers/enroll/src.rs
@@ -12,7 +12,7 @@ use base64::prelude::*;
 use std::convert::TryFrom;
 use openssl::{
     x509::{X509, X509Builder, X509NameBuilder},
-    pkey::PKey,
+    pkey::{PKey, Id as PKeyId},
     asn1::Asn1Time,
     hash::MessageDigest,
 };
@@ -88,11 +88,11 @@ pub async fn enroll_handler(
     builder.set_pubkey(&pubkey).map_err(to_http_err)?;
     // builder.sign(&ca_key, MessageDigest::null()).map_err(to_http_err)?;
     
-    let digest = match pubkey.id() {
-        openssl::pkey::Id::EC | openssl::pkey::Id::RSA => MessageDigest::sha256(),
-        _ => MessageDigest::sha256(),
+    let ca_digest = match ca_key.id() {
+        PKeyId::ED25519 => MessageDigest::null(),  // Ed25519 - no external hash
+        _               => MessageDigest::sha256(),// ECDSA-P256, RSA, etc.
     };
-    builder.sign(&ca_key, digest).map_err(to_http_err)?;
+    builder.sign(&ca_key, ca_digest).map_err(to_http_err)?;
 
     sqlx::query!(
         r#"
